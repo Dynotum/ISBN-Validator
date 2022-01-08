7b53a3197a38592b711b817b8cae6ac4d818bb23
Carlos Sedano <carlos.sedano@oracle.com>
2022-01-07 23:10:32 -0600

    added first red-green-refactor methods

--- src/main/java/ValidatorISBN.java
+++ src/main/java/ValidatorISBN.java
@@ -1,3 +1,9 @@
 public class ValidatorISBN {
 
+    public boolean checkISBN(int isbn) {
+        if (isbn == 134685997) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/ValidatorISBNTest.java
+++ src/test/java/ValidatorISBNTest.java
@@ -1,5 +1,21 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ValidatorISBNTest {
+
+    @Test
+    public void checkValidatorISBN() {
+        ValidatorISBN validatorISBN = new ValidatorISBN();
+        boolean result = validatorISBN.checkISBN(134685997);
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkInvalidValidatorISBN() {
+        ValidatorISBN validatorISBN = new ValidatorISBN();
+        boolean result = validatorISBN.checkISBN(134685999);
+        assertFalse(result);
+    }
 
 }
